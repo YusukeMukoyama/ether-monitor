1e71a0103492243ec28b9c4da3de93906235e2cc
YusukeMukoyama <yusuke.mukoyama@gmail.com>
2016-04-16 14:01:12 +0900

    prepare Deploy contract

---
+++ client/templates/contract_deploy.js
@@ -0,0 +1,11 @@
+/*
+Template.contractDeploy.helpers({
+
+  currentProvider: function(){  //ファンクション名を記載 -> このファンクション名をtempleteのhtmlファイルが用いる
+    return web3.currentProvider.host; //web3のメソッドを記載
+  },
+  isMining: function(){
+    return Session.get('isMining');
+  }
+});
+*/
